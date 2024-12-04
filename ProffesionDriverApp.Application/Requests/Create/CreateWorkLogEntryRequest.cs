c09f580dfb7e7d364658806477b16cbeb91b4d41
Arturbul <artur.bula2001@gmail.com>
2024-12-04 15:04:41 +0100

    new model

---
+++ ProffesionDriverApp.Application/Requests/Create/CreateWorkLogEntryRequest.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProfessionDriverApp.Application.Requests.Create
+{
+    public class CreateWorkLogEntryRequest
+    {
+        [StringLength(12)]
+        public string RegistrationNumber { get; set; } = null!;
+        [Required]
+        public DateTime LogTime { get; set; }
+        public string? Place { get; set; }
+        public float? Mileage { get; set; }
+        public string? DriverUserName { get; set; } //if not attached, get userName form JWT
+    }
+}
