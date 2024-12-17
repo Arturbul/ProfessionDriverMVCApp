fb9a4c07f95cbdfe623d93a6f6745552f32d1a5b
Arturbul <artur.bula2001@gmail.com>
2024-12-17 19:40:27 +0100

    update swagger filter type method

---
+++ ProffesionDriverApp.Domain/ValueObjects/FilterType.cs
@@ -0,0 +1,21 @@
+﻿using System.Runtime.Serialization;
+using System.Text.Json.Serialization;
+
+namespace ProfessionDriverApp.Domain.ValueObjects
+{
+    [JsonConverter(typeof(JsonStringEnumConverter))] // Konwersja JSON między string a enum
+    public enum FilterType
+    {
+        [EnumMember(Value = "last7Days")]
+        Last7Days,
+
+        [EnumMember(Value = "currentMonth")]
+        CurrentMonth,
+
+        [EnumMember(Value = "currentYear")]
+        CurrentYear,
+
+        [EnumMember(Value = "custom")]
+        Custom
+    }
+}
